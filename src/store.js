a0d78aca0b8231d345bea4e43a371c6cc393972f
Kristoffer Brabrand <kristoffer@brabrand.no>
2018-04-27 15:25:50 +0200

    Start of tilbygg schema

--- src/store.js
+++ src/store.js
@@ -3,12 +3,13 @@
 import { createStore, combineReducers, compose } from 'redux';
 import { state } from 'losen';
 import { persistStore, autoRehydrate } from 'redux-persist';
+import schema from './api/bus-tilbygg.json';
 
 /**
  * Create the store with middleware
  */
 const store = createStore(
-  combineReducers({ [state.NAME]: state.reducer }),
+  combineReducers({ [state.NAME]: state.reducer(schema) }),
   undefined,
   compose(autoRehydrate(), window.devToolsExtension ? window.devToolsExtension() : f => f),
 );
